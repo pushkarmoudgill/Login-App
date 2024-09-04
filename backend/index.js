409f247c031c34c4f3b4d3b812c2401e10c177c8
pushkarmoudgill <106948853+pushkarmoudgill@users.noreply.github.com>
2024-09-04 06:01:42 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,13 @@ const PORT=process.env.PORT || 8000;
 
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+    {
+        origin: ["https://login-app-ui.vercel.app/"],
+        methods: ["POST","GET"],
+        credentials: true
+    }
+));
 app.use(express.urlencoded({extended: true}));
 DBConnection();
 
